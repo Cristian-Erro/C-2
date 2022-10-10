9f296dab93891bab71881f0beb47e40487463f53
Cristian Erro <cristianlp86@gmail.com>
2022-10-10 19:01:01 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string nombre;
+            Console.WriteLine("Ingrese su nombre completo");
+            nombre=Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;  
+            Console.BackgroundColor = ConsoleColor.Red; 
+            Console.WriteLine("///Bienvenido "+nombre+"//");
+            Console.ResetColor();
+
+        }
+    }
+}
